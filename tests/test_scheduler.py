97528d2a25c86efa13af2f65582063c1cbab68ab
Lucas Esclapez <lesclapez@esciencecenter.nl>
2024-11-11 10:56:33 +0100

    Make sure oversubscribing scheduler test fails.

--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -1,6 +1,8 @@
 """Tests for the gut_scheduler class."""
 import pytest
+import numpy as np
 import os
+import psutil
 import toml
 from guts_utils.guts_scheduler import guts_scheduler
 
@@ -26,10 +28,11 @@ def test_start_scheduler():
 
 def test_oversubscribe_scheduler():
     """Test oversubscribing a scheduler."""
+    ncpus = np.ceil(psutil.cpu_count()/4) + 1
     with open("input.toml", 'w') as f:
         toml.dump({"case": {"name": "test"},
                    "resource": {"nwgroups": 4,
-                                "config": {"nworkers": 3, "runtime": 1}}}, f)
+                                "config": {"nworkers": ncpus, "runtime": 1}}}, f)
     guts_sched = guts_scheduler(a_args=[])
     with pytest.raises(Exception):
         guts_sched.start()
