1ce0efacb2491ad8024f0eee96b7f36d1efca8d8
Lucas Esclapez <lesclapez@esciencecenter.nl>
2025-01-06 18:51:25 +0100

    Add non-slurm only tests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,3 +6,9 @@ def slurm_available():
     """Fixture to check SLURM availability."""
     if not is_slurm_avail():
         pytest.skip("SLURM is not available on this system. Skipping SLURM-dependent tests.")
+
+@pytest.fixture(scope="session")
+def slurm_not_available() -> None:
+    """Fixture to check SLURM unavailability."""
+    if is_slurm_avail():
+        pytest.skip("SLURM is available on this system. Skipping SLURM-missing tests.")
--- tests/test_resource_manager.py
+++ tests/test_resource_manager.py
@@ -3,29 +3,33 @@ import pytest
 import os
 import time
 import psutil
+import pathlib
 
-from guts_utils.guts_resource_manager import resource_manager, resource_set_slurm
+from guts_utils.guts_resource_manager import resource_manager, resource_set_slurm, resource_set_local
 
 def test_init_manager():
     """Test creating a resource manager."""
-    config = {"nwgroups": 1}
+    config = {"resource" : {"nwgroups": 2}}
     manager = resource_manager(config)
-    assert(manager.get_nwgroups() == 1)
+    assert(manager.get_nwgroups() == 2)
 
 def test_init_manager_unknown_backend():
     """Test creating a resource manager with unknown backend"""
-    config = {"nwgroups": 1, "resource" : {"backend" : "WillFail"}}
+    config = {"resource" : {"nwgroups": 1, "backend" : "WillFail"}}
     manager = resource_manager(config)
     with pytest.raises(Exception):
-        _ = manager.get_resources({}, None, 0)
+        _ = manager.get_resources({}, 0)
 
-def test_get_resources():
+def test_get_resources(slurm_not_available):
     """Test getting the resources from the manager."""
-    config = {"nwgroups": 1}
+    config = {"case" : {"name" : "RSTests"},
+              "resource" : {"nwgroups": 1}}
     manager = resource_manager(config)
     res_config = {"nworkers": 2, "runtime": 1}
-    resources = manager.get_resources(res_config, None, 0)
+    resources = manager.get_resources(res_config, 0)
+    time.sleep(0.5)
     assert(resources.get_nworkers() == 2)
+    pathlib.Path.unlink("./RSTests_queue.db")
 
 def test_oversubscribe_resources():
     """Test getting too many resources from the manager."""
@@ -33,14 +37,42 @@ def test_oversubscribe_resources():
     manager = resource_manager(config)
     res_config = {"nworkers": psutil.cpu_count() + 1}
     with pytest.raises(Exception):
-        resources = manager.get_resources(res_config, None, 0)
+        resources = manager.get_resources(res_config, 0)
+
+def test_define_local_resource_set(slurm_not_available):
+    """Test initializing a local resource set."""
+    res_config = {"nworkers": 2, "runtime": "10"}
+    res = resource_set_local(15, res_config = res_config)
+    assert res.get_nworkers() == 2
+
+def test_serialize_local_resource_set(slurm_not_available):
+    """Test initializing a local resource set."""
+    res_config = {"nworkers": 2, "runtime": "10"}
+    res = resource_set_local(10000, res_config = res_config)
+    serialized_res = res.serialize()
+    res_restored = resource_set_local(10000, json_str = serialized_res)
+    assert res_restored.get_nworkers() == 2
 
 def test_define_slurm_resource_set(slurm_available):
-    res_config = {"nworkers": 1}
-    res = resource_set_slurm(res_config, 0)
+    """Test initializing a slurm resource set."""
+    res_config = {"nworkers": 7, "nodes": 1, "runtime": "30"}
+    res = resource_set_slurm(12, res_config = res_config)
+    assert res.get_nworkers() == 7
 
+def test_serialize_slurm_resource_set(slurm_available):
+    """Test serializing a slurm resource set."""
+    res_config = {"nworkers": 12, "nodes": 1, "runtime": "1:30"}
+    res = resource_set_slurm(12, res_config = res_config)
+    serialized_res = res.serialize()
+    res_restored = resource_set_slurm(12, json_str = serialized_res)
+    assert res_restored.get_nworkers() == 12
+    
 def test_get_resources_slurm(slurm_available):
+    """Test getting a slurm resource set through the manager."""
     config = {"resource" : {"nwgroups": 1, "backend": "slurm"}}
     manager = resource_manager(config)
-    res_config = {"nworkers": 2, "runtime": 10}
-    resources = manager.get_resources(res_config, None, 0)
+    res_config = {"nodes": 2, "runtime": "00:10",
+                  "extra_directives": {"--exclusive" : None}}
+                                       #"--hold": None}}
+    resources = manager.get_resources(res_config, 0)
+    #resources.release()
